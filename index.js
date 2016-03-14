fa455425da27fb67b7551b571ace9c7dda05a566
abe33 <cedric.nehemie@gmail.com>
2016-03-14 17:05:47 +0100

    Remove button after toggling collapse

--- index.js
+++ index.js
@@ -6,5 +6,6 @@ window.onload = function () {
   viewMoreOffers.addEventListener('click', function (e) {
     e.preventDefault()
     document.querySelector(viewMoreOffers.getAttribute('data-target')).classList.add('in')
+    viewMoreOffers.parentNode.removeChild(viewMoreOffers)
   })
 }
